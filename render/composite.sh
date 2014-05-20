08858a2586ef28632950963e0ea7acf845d2ae56
Alex Cornejo <acornejo@gmail.com>
2014-05-20 09:14:39 -0400

    change names

--- render/composite.sh
+++ render/composite.sh
@@ -2,25 +2,33 @@
 
 CONVERT=$(which convert)
 
-BOARD_COLOR="green"
+BOARD_COLOR="purple"
 COPPER_COLOR="gold"
-MASK_COLOR="green"
+MASK_COLOR="purple"
 MASK_OPACITY="70"
 SILK_COLOR="white"
 HOLE_COLOR="black"
 
-${CONVERT} eagle_outline.png -transparent white -fill ${BOARD_COLOR} -colorize "100%" \
-\( eagle_top.png eagle_drill.png -compose Plus -composite -write mpr:copper -delete 0,2 \
-\( mpr:copper -transparent-color white -background ${COPPER_COLOR} -alpha Shape \) \
-\( mpr:copper -shade 120x30 -blur 0x1 -auto-level mpr:copper -compose CopyOpacity -composite \) -compose Overlay -composite \) -compose Atop -composite \
-\( eagle_top_mask.png -negate -brightness-contrast -${MASK_OPACITY} -background ${MASK_COLOR} -alpha Shape \) -compose Atop -composite \
-\( \( eagle_top_silk.png \( eagle_top_mask.png -negate \) -compose Multiply -composite \) -transparent-color black -background ${SILK_COLOR} -alpha Shape \) -compose Atop -composite \
-\( eagle_drill.png -transparent-color black -background ${HOLE_COLOR} -alpha Shape \) -compose Atop -composite board_top.png
 
-${CONVERT} eagle_outline.png -transparent white -fill ${BOARD_COLOR} -colorize "100%" \
-\( eagle_bottom.png eagle_drill.png -compose Plus -composite -write mpr:copper -delete 0,2 \
+BOARD_COLOR="rgb(58,18,100)"
+COPPER_COLOR="rgb(177,136,131)"
+MASK_COLOR="rgb(58,18,100)"
+MASK_OPACITY="10"
+SILK_COLOR="rgb(193,177,203)"
+HOLE_COLOR="black"
+
+${CONVERT} board_outline.png -transparent white -fill ${BOARD_COLOR} -colorize "100%" \
+\( board_top.png board_drill.png -compose Plus -composite -write mpr:copper -delete 0,2 \
 \( mpr:copper -transparent-color white -background ${COPPER_COLOR} -alpha Shape \) \
 \( mpr:copper -shade 120x30 -blur 0x1 -auto-level mpr:copper -compose CopyOpacity -composite \) -compose Overlay -composite \) -compose Atop -composite \
-\( eagle_bottom_mask.png -negate -brightness-contrast -${MASK_OPACITY} -background ${MASK_COLOR} -alpha Shape \) -compose Atop -composite \
-\( \( eagle_bottom_silk.png \( eagle_bottom_mask.png -negate \) -compose Multiply -composite \) -transparent-color black -background ${SILK_COLOR} -alpha Shape \) -compose Atop -composite \
-\( eagle_drill.png -transparent-color black -background ${HOLE_COLOR} -alpha Shape \) -compose Atop -composite -flop board_bottom.png
+\( board_top_mask.png -negate -brightness-contrast -${MASK_OPACITY} -background ${MASK_COLOR} -alpha Shape \) -compose Atop -composite \
+\( \( board_top_silk.png \( board_top_mask.png -negate \) -compose Multiply -composite \) -transparent-color black -background ${SILK_COLOR} -alpha Shape \) -compose Atop -composite \
+\( board_drill.png -transparent-color black -background ${HOLE_COLOR} -alpha Shape \) -compose Atop -composite board_top_render.png
+
+${CONVERT} board_outline.png -transparent white -fill ${BOARD_COLOR} -colorize "100%" \
+\( board_bottom.png board_drill.png -compose Plus -composite -write mpr:copper -delete 0,2 \
+\( mpr:copper -transparent-color white -background ${COPPER_COLOR} -alpha Shape \) \
+\( mpr:copper -shade 120x30 -blur 0x1 -auto-level mpr:copper -compose CopyOpacity -composite \) -compose Overlay -composite \) -compose Atop -composite \
+\( board_bottom_mask.png -negate -brightness-contrast -${MASK_OPACITY} -background ${MASK_COLOR} -alpha Shape \) -compose Atop -composite \
+\( \( board_bottom_silk.png \( board_bottom_mask.png -negate \) -compose Multiply -composite \) -transparent-color black -background ${SILK_COLOR} -alpha Shape \) -compose Atop -composite \
+\( board_drill.png -transparent-color black -background ${HOLE_COLOR} -alpha Shape \) -compose Atop -composite -flop board_bottom_render.png
--- render/export.sh
+++ render/export.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 EAGLE=/Applications/EAGLE-6.5.0/EAGLE.app/Contents/MacOS/EAGLE
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+rm -f board_*.png
 ${EAGLE} -C 'SCRIPT imagedump.scr; QUIT' picoavr.brd
+. ${DIR}/composite.sh
